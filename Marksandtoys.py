e36aabb1790815bad9089d01f7cd0535ee355953
LouisaKorshunova <korshunova.2053311@studenti.uniroma1.it>
2024-11-17 21:55:29 +0100

    Create Marksandtoys.py

---
+++ Marksandtoys.py
@@ -0,0 +1,46 @@
+#Marks and toys
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'maximumToys' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY prices
+#  2. INTEGER k
+#
+
+def maximumToys(prices, k):
+    prices.sort()
+    total_cost = 0
+    num_toys = 0
+    for price in prices:
+        if total_cost + price <= k:
+            total_cost += price
+            num_toys += 1
+        else:
+            break 
+    
+    return num_toys
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    k = int(first_multiple_input[1])
+
+    prices = list(map(int, input().rstrip().split()))
+
+    result = maximumToys(prices, k)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
